340d15d27c21fca690dc137be127e6691bb4dd82
Adam Feher <fhradm@gmail.com>
2018-03-30 22:51:18 +0200

    hello

--- static/js/main.js
+++ static/js/main.js
@@ -14,4 +14,20 @@ $(document).keydown(function(e) {
         $("#btn")[0].click();
         return false;
     }
+    if(e.keyCode === 87) {
+        $("#sb_vs_btn")[0].click();
+        return false;
+    }
+    if(e.keyCode ===67) {
+        $("#bb_vs_btn")[0].click();
+        return false;
+    }
+    if(e.keyCode === 82) {
+        $("#bvb")[0].click();
+        return false;
+    }
+    if(e.keyCode===84) {
+        $("#hu")[0].click();
+        return false;
+    }
 });
